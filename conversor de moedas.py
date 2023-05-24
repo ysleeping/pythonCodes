317d32ea0d16144bcf18ec1fb67d6dd3d116e644
Raphael Leandro de Souza Barbosa <114081816+ysleeping@users.noreply.github.com>
2023-05-24 14:08:41 -0300

    01

--- conversor de moedas.py
+++ conversor de moedas.py
@@ -1,27 +1,39 @@
 print("-"*26)
-print("digite a opção de conversão")
+print("Digite a opção de conversão")
 print("-"*26)
-print('a- real-> dolar\n''b  real-> euro\n''c- real-> ouro')
-print('d- dolar-> real\n''e- euro-> real\n''f- ouro-> real')
+print('a- real-> dolar\n''b  real-> euro\n''c- real-> libra')
+print('d- dolar-> real\n''e- euro-> real\n''f- libra-> real')
 print("-"*14)
 moeda = input()
-while moeda not in ('a', 'b', 'c','d','e','f'):
-    moeda = input("por favor, digite uma alternativa correta\n")
+while moeda not in ('a', 'b', 'c', 'd', 'e', 'f'):
+    moeda = input("Por favor, digite uma alternativa correta\n")
 
-print("quanto você quer converter ? ")
+print("Quanto você quer converter ? ")
 
 #quantidade da moeda que vai ser convertida
 quantidade = float(input())
 
 #valores de cada moeda em real.
-dolar = 4.97
+dolar = 4.95
 euro = 5.36
-ouro = 315.85
+libra = 6.12
 
 if moeda == 'a':
     print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade/dolar):,.2f} doláres")
+
 if moeda == 'b':
-    print("Você escolheu a conversão de real para euro")
+    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade/euro):,.2f} euros")
 
 if moeda == 'c':
-    print("Você escolheu a conversão de real para ouro")
+    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade/libra):,.2f} libras")
+
+if moeda == 'd':
+    print(f"A quantidade convertida foi de {quantidade} dólares para {(quantidade*dolar):,.2f} reais")
+if moeda == 'e':
+    print(f"A quantidade convertida foi de {quantidade} euros para {(quantidade*euro):,.2f} reais")
+if moeda == 'f':
+    print(f"A quantidade convertida foi de {quantidade} libras para {(quantidade*libra):,.2f} reais")
+
+print("-"*62)
+
+
