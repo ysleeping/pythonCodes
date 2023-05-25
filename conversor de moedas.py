12ea96518b4e81f7021215a4273860b80de92d0c
Raphael Leandro de Souza Barbosa <114081816+ysleeping@users.noreply.github.com>
2023-05-25 10:05:33 -0300

    Conversor

--- conversor de moedas.py
+++ conversor de moedas.py
@@ -1,37 +1,41 @@
 print("-"*26)
 print("Digite a opção de conversão")
-print("-"*26)
+print("-" * 26)
 print('a- real-> dolar\n''b  real-> euro\n''c- real-> libra')
 print('d- dolar-> real\n''e- euro-> real\n''f- libra-> real')
-print("-"*14)
-moeda = input()
-while moeda not in ('a', 'b', 'c', 'd', 'e', 'f'):
-    moeda = input("Por favor, digite uma alternativa correta\n")
+print("-" * 14)
 
-print("Quanto você quer converter ? ")
+# coin é a moeda que vai ser convertida
+coin = input()
+while coin not in ('a', 'b', 'c', 'd', 'e', 'f'):
+    coin = input("Por favor, digite uma alternativa correta\n")
 
-#quantidade da moeda que vai ser convertida
+print("Digite a quantidade que você irá converter: ")
+
+# quantidade da moeda que vai ser convertida
 quantidade = float(input())
 
 #valores de cada moeda em real.
-dolar = 4.95
+dol = 4.95
 euro = 5.36
 libra = 6.12
 
-if moeda == 'a':
-    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade/dolar):,.2f} doláres")
+if coin == 'a':
+    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade / dol):,.2f} doláres")
 
-if moeda == 'b':
-    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade/euro):,.2f} euros")
+if coin == 'b':
+    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade / euro):,.2f} euros")
 
-if moeda == 'c':
-    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade/libra):,.2f} libras")
+if coin == 'c':
+    print(f"A quantidade convertida foi de {quantidade} reais para {(quantidade / libra):,.2f} libras")
 
-if moeda == 'd':
-    print(f"A quantidade convertida foi de {quantidade} dólares para {(quantidade*dolar):,.2f} reais")
-if moeda == 'e':
+if coin == 'd':
+    print(f"A quantidade convertida foi de {quantidade} dólares para {(quantidade*dol):,.2f} reais")
+
+if coin == 'e':
     print(f"A quantidade convertida foi de {quantidade} euros para {(quantidade*euro):,.2f} reais")
-if moeda == 'f':
+
+if coin == 'f':
     print(f"A quantidade convertida foi de {quantidade} libras para {(quantidade*libra):,.2f} reais")
 
 print("-"*62)
