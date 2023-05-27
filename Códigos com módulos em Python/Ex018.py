ea18c7fb60818514f9e62ba10ce202c1de72e85a
Raphael Leandro de Souza Barbosa <114081816+ysleeping@users.noreply.github.com>
2023-05-26 21:44:36 -0300

    Conversor

--- Códigos com módulos em Python/Ex018.py
+++ Códigos com módulos em Python/Ex018.py
@@ -1,6 +1,6 @@
 import math
 angulo = int(input("Digite o ângulo para que seja dado o seno, cosseno e a tangente: "))
 
-print(f'O ângulo dado foi de {angulo}º\nSeno: {math.sin(math.radians(angulo))}\n'
-      f'Cosseno: {math.cos(math.radians(angulo))}\n'
-      f'Tangente: {math.tan(math.radians(angulo))}')
+print(f'O ângulo dado foi de {angulo}º\nSeno: {(math.sin(math.radians(angulo))):,.3f}\n'
+      f'Cosseno: {(math.cos(math.radians(angulo))):,.3f}\n'
+      f'Tangente: {(math.tan(math.radians(angulo))):,.3f}')
