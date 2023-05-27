0462c620e4116b64c79e243bd1d24859cc4b2da1
Raphael Leandro de Souza Barbosa <114081816+ysleeping@users.noreply.github.com>
2023-05-26 21:40:27 -0300

    Conversor

--- Códigos com módulos em Python/Ex017.py
+++ Códigos com módulos em Python/Ex017.py
@@ -1,6 +1,7 @@
 import math
-catOP = 3
-catAD = 2
-hip = 1
 
-print(f'{math.pow(catAD,2)}')
+catOP = float (input("Digite o valor do cateto oposto: "))
+catAD = float (input("Digite o valor do cateto adjacente: "))
+hip =  math.sqrt((math.pow(catOP,2)) + (math.pow(catAD,2)))
+print(f'O valor da hipotenusa é : {hip} cm')
+
