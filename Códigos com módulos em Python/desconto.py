923b31dc46e5e188e48b0183fa5aab5010d0a2e7
Raphael Leandro de Souza Barbosa <114081816+ysleeping@users.noreply.github.com>
2023-05-31 11:20:06 -0300

    Conversor

--- Códigos com módulos em Python/desconto.py
+++ Códigos com módulos em Python/desconto.py
@@ -1,5 +1,21 @@
 ValorDoProduto = float (input("Digite o valor do produto: "))
+frase = 'curso em video python'
+frase.upper()
 desconto = 0.05 * ValorDoProduto
 ValorComDesconto = ValorDoProduto - desconto
 print('A loja está com uma promoção de 5% de desconto !!!')
 print(f'Por conta disso o valor do seu produto passou de {ValorDoProduto:,.2f} reais para {ValorComDesconto:,.2f} reais, seu desconto foi de {desconto:,.2f} reais')
+print(frase)
+
+print(len(frase))
+print('')
+print(frase.count('o'))
+print('')
+print(frase.count('o', 0, 14))
+print('')
+print(frase.find('deo'))
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import time
-print("olá mundo")
-print("???: Se você iniciou esse programa agora, digite enter para continuar...")
 
+print("olá mundo")
+print('                                           The F*cking End                                                     ')
+print('Esse é um programa de teste do meu conhecimento em python, estou me baseando em um rpg de multiplas escolhas e '
+      'multiplos caminhos e tentar conseguir criar isso ao máximo e mais próximo de um rpg de mesa ou algo do tipo'
+      'espero que gostem ou aproveitem, o desenvolvimento é lento graças a minha falta de vergonha na cara'
+      'Bom "Jogo" kkkkkk')
+print("???: Se você iniciou esse programa agora, digite enter para continuar...")
 input(" ")
+name = input('Digite teu nome, jovem padawan: ')
+level = 1
+hp = 100
 time.sleep(5)
 
 print("???: talvez você tenha duvidas do que está acontecendo, sei que pode ser estranho mas temos que conversar...\n")
@@ -20,24 +28,49 @@ if resposta == "a":
 if resposta == "b":
     print("???: Talvez você ainda não tenha coragem de interagir, mas não tenha medo, sou mais confiante do que você imagina!\n")
 
-resposta1 = input("Você: ")
+time.sleep(5)
+print('a- O que está acontecendo?'
+      '\nb- Mas que porra é essa?'
+      '\nc- Continuar sem responder'
+      '\nDigite uma das opções...')
 
-if resposta1 =="o que está acontecendo ?":
+resposta1 = input("Você: ")
+time.sleep(5)
+if resposta1 == "a":
     print("\n???: Eu sou você no futuro, assim como todos ja imaginam, as máquinas realmente venceram.\n"
           "No fim nós tivemos que evoluir também e agora estamos numa simbiose com as máquinas que ainda podemos controlar, a internet se torno a rachadura no espaço tempo,\n"
           "por esse motivo é muito fácil transmitir pensamentos do futuro ou interagir com outras pessoas\n")
-else:
-    print("\n???: Você parece não querer saber muito, talvez tenha duvidas sobre o que está ou vai acontecer,\nnessa época eu estava depressivo pois minha vida não se tornou o que eu imaginava,\n"
-          "mas se quiser um spoiler, vai dar tudo certo, mesmo que não seja perfeito ainda vai dar tudo certo !\n")
 
+if resposta1 == "b":
+    print("\n???: Eu sou você no futuro, assim como todos ja imaginam, as máquinas realmente venceram.\n"
+          "No fim nós tivemos que evoluir também e agora estamos numa simbiose com as máquinas que ainda podemos controlar,"
+          " a internet se torno a rachadura no espaço tempo,\n"
+          "por esse motivo é muito fácil transmitir pensamentos do futuro ou interagir com outras pessoas\n")
+else:
+    print("\n???: Você parece não querer saber muito, talvez tenha duvidas sobre o que está ou vai acontecer,"
+          "\nnessa época eu estava depressivo pois minha vida não se tornou o que eu imaginava,\n"
+            "mas se quiser um spoiler, vai dar tudo certo, mesmo que não seja perfeito ainda vai dar tudo certo !\n")
+
+time.sleep(5)
+print('a- isso é real ?'
+      '\nb- como posso saber se você está falando a verdade ?'
+      '\nc- Porra, isso não é de um filme ? Matrix ?'
+      '\nDigite uma das opções...')
 resposta2 = input("Você: ")
+time.sleep(5)
+if resposta2 == "a":
+    print("\n???: O que é real? Como você define o real? Se você está falando sobre o que você pode sentir, o que você pode cheirar, "
+          "o que você pode saborear e ver, o real são simplesmente sinais elétricos interpretados pelo seu cérebro.")
 
-if resposta2 == "isso é real ?":
-    print("\n???: O que é real ? Como se define o real \nVocê saberia diferenciar algo real e inreal simplesmente pelo toque ?\n")
-
-elif resposta2 == "como posso saber se você está falando a verdade ?":
-    print("\n???: Sei que você tem 25 anos, vai completar 26 esse ano,\ne que no dia de hoje desceu para fumar e encontrou uma gata perto do local e foi fazer carinho nela,\n"
-          "ela mordeu você e arranhou sua mão e hoje você irá sair com seus amigos pois é dia da marcha pró descriminalização da maconha")
+if resposta2 == "b":
+    print("\n???: Hahahaha, como assim ? eu posso ser apenas um delírio da sua mente, "
+          "você não pode considerar nada disso ou qualquer outra coisa como real seu idiota.")
 else:
-    print("\n???: Você até agora não consegue comprender a verdade")
+    print("\n???: Você deveria fazer as perguntas certas e não perder tempo supondo as coisas...")
 
+print('???: Eu realmente estou sem paciência para continuar tal coisa, então vamos adicionar uma coisinha nessa brincadeira, '
+      'você irá receber um número de hp(valor de vida) e um nível, seu hp ira começar em 100 e seu nível será 1,'
+      ' cada vez que você fiz uma pergunta errada vai ')
+
+print(f'Dados do jogador: \nNome do jogador: {name}\nLevel do jogador: {level}\nHP do jogador: {hp}')
+print('???: Agora irá ficar divertido hahahahaha')
